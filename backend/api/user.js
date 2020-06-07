0b800f4ddddc1242d09fea8f4572e8c82c593275
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-06-07 16:17:29 -0300

    API USER = Organização

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,73 +1,64 @@
 const bcrypt = require('bcrypt-nodejs')
 
 module.exports = app => {
-	const { existsOrError, notExistsOrError } = app.api.validation
+	const { existsOrError, notExistsOrError, existsInDatabase } = app.api.validation
 
-
-	const saveOrUpdate = async (req, res) => {
-		
-		//DADOS ENVIADOS SALVOS EM UMA CONSTANTE
+	const saveOrUpdate = async (req, res) => {		
 		const user = { ...req.body }
 		
-		
-		//CRIPTOGRAFAR A SENHA
 		const encryptPassword = password => {
 			let salt = bcrypt.genSaltSync(10)
 			return bcrypt.hashSync(password, salt)
 		}
 
-		//POSSUI CAMPO VAZIO ?
-		try {
-			existsOrError(user.username, 'Usuário não definido!')
-			existsOrError(user.email, 'Email não definido!')
-			existsOrError(user.password, 'Defina uma senha!')
-			existsOrError(user.confirmPassword, 'Repita sua senha!')
-			existsOrError(user.age, 'Idade não definido!')
-			existsOrError(user.full_name, 'Nome Completo não definido!')
-			if(user.password !== user.confirmPassword) throw 'Senhas não conferem!'
-
-
-		} catch (error) {
-			return res.status(400).send(error)
-		}
-
-		//TRANSFORMAÇÂO DO USUARIO & EMAIL
+		console.log(await existsInDatabase('user_id', 'user_id', 1))
+		//TRANSFORMAÇÂO DADOS EM MINUSCULO
 		user.username = user.username.toLowerCase()
 		user.email = user.email.toLowerCase()
 
-		//VALIDAÇÂO DE EXISTENCIA NO BANCO DE DADOS
-		existInDatabase = (column, value) => {
-			return app.db('users')	
-					.select(column)
-					.where({ email: value })
-					.orWhere({ username: value })
-					.first()
+		//CREATE
+		if(!user.user_id){
+			//VALIDACAO PARA CREATE
+			try {
+				existsOrError(user.username, 'Usuário não definido!')
+				existsOrError(user.email, 'Email não definido!')
+				existsOrError(user.password, 'Defina uma senha!')
+				existsOrError(user.confirmPassword, 'Repita sua senha!')
+				existsOrError(user.age, 'Idade não definido!')
+				existsOrError(user.full_name, 'Nome Completo não definido!')
+				if(user.password !== user.confirmPassword) throw 'Senhas não conferem!'
+
+
+			} catch (error) {
+				return res.status(400).send(error)
+			}
 		}
 
+		//CRIPTOGRAFAR SENHA E RETIRAR A CONFIRMAÇAO ANTES DO ENVIO
+		user.password = encryptPassword(user.password)
+		delete user.confirmPassword
+		
 		try {
-			//VALIDAÇÂO
-			notExistsOrError(await existInDatabase('username', user.username), 'Usuário cadastrado!')
-			notExistsOrError(await existInDatabase('email', user.email), 'Email cadastrado!')
-
-			//CRIPTOGRAFAR SENHA E RETIRAR A CONFIRMAÇAO ANTES DO ENVIO
-			user.password = encryptPassword(user.password)
-			delete user.confirmPassword
-
 			//SERA UM UPDATE OU CREATE ???
-
-			if(user.user_id) { 		//UPDATE
+			if(user.user_id) { 
+				//UPDATE
 				app.db('users')
 					.update(user)
 					.where({user_id: user.user_id})
-					.then(()=> res.status(204).send('Atualizado!'))
+					.then(()=> res.status(204).send())
 					.catch(error=> res.status(500).send('ERRO AO ATUALIZAR!'))
-				console.log('ATUALIZADO')
-			} else { 				//CREATE
+				return res.send('Update com sucesso')
+			} else { 	
+				//VALIDAÇÂO
+				notExistsOrError(await existsInDatabase('username','username', user.username), 'Usuário já está cadastrado!')
+				notExistsOrError(await existsInDatabase('email','email', user.email), 'Email já cadastrado!')
+				//CREATE
 				app.db('users')
 					.insert(user)
 					.then(() => res.status(204).send())
 					.catch(error => res.status(500).send(error))
-				console.log('CRIADO')
+				return res.send('Criado com sucesso')
+	
 			}
 		} catch (error) {
 			return res.status(201).send(error)
@@ -75,19 +66,19 @@ module.exports = app => {
 		
 
 	}
-	const remove = (req, res) => {
+	const removeUser = async (req, res) => {
 		user = { ...req.body }
-
+		console.log(user)
 		try {
 			existsOrError(user.user_id, 'É necessário o ID do usuário')
-			app.db('users')
-				.where({ user_id: user.user_id })
-				.del()
+			await app.db('users')
+					.where({ user_id: user.user_id })
+					.del()
 		} catch(error) {
 			return res.status(400).send(error)
 		}
-	
+		return res.send('Usuário apagado')
 	
 	}
-	return { saveOrUpdate, remove }
+	return { saveOrUpdate, removeUser }
 }

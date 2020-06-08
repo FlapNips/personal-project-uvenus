bb80ee619009aa0d1687ba0d964d1e8c63242ed4
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-06-07 21:02:37 -0300

    Update 20200606170232_create_table_articles.js

--- backend/database/migrations/20200606170232_create_table_articles.js
+++ backend/database/migrations/20200606170232_create_table_articles.js
@@ -1,12 +1,13 @@
 exports.up = function(knex) {
   return knex.schema.createTable('articles', table => {
-	table.increments('article_id').primary()
-	table.string('article_name', 50).notNull()
-	table.string('article_img', 1000)
-    table.string('description', 1000).notNull()
-    table.binary('content').notNull()
-    table.integer('category_parent').references('category_id')
+    table.increments('article_id').primary()
+    table.string('article_name', 50).notNull()
+    table.string('article_img', 1000)
+    table.string('article_description', 1000).notNull()
+    table.binary('article_content').notNull()
+    table.integer('article_category_parent').references('category_id')
             .inTable('categories').notNull()
+    table.string('article_created_in').notNull()
   })
 };
 

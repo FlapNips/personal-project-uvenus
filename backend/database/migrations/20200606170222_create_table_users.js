04d8999ed621b6e8cba6de90bf8c8ccddb288ddd
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-06-07 21:02:32 -0300

    Update 20200606170222_create_table_users.js

--- backend/database/migrations/20200606170222_create_table_users.js
+++ backend/database/migrations/20200606170222_create_table_users.js
@@ -1,15 +1,19 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('users', table => {
-	table.increments('user_id').primary()
-	table.string('username', 25).notNull().unique()
-	table.string('full_name', 50).notNull()
-	table.integer('age', 3).notNull()
-	table.string('email', 40).notNull().unique()
-	table.string('password', 150).notNull()
-	table.integer('points').notNull().defaultTo(0)
-	table.integer('rank').defaultTo(1).references('rank_id')
-		.inTable('ranks').notNull()
-	table.boolean('admin').notNull().defaultTo(false)
+exports.up = async function(knex) {
+	return knex.schema.createTable('users', table => {
+		table.increments('user_id').primary()
+		table.string('username', 25).notNull().unique()
+		table.string('full_name', 50).notNull()
+		table.integer('age', 3).notNull()
+		table.string('email', 40).notNull().unique()
+		table.string('password', 150).notNull()
+		table.integer('points').notNull().defaultTo(0)
+		table.integer('rank').defaultTo(1).references('rank_id')
+			.inTable('ranks').notNull()
+		table.boolean('admin').notNull().defaultTo(false)
+		table.string('created_in').notNull()
+		table.string('last_update')
+		table.integer('banned_time')
+		table.boolean('deleted_at')
   })
 };
 

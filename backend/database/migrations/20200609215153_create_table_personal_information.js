3926c4cd344e01c387833906b6cc5ed8bd587d9b
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-06-08 22:26:25 -0300

    Create 20200609215153_create_table_personal_information.js

---
+++ backend/database/migrations/20200609215153_create_table_personal_information.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+	return knex.schema.createTable('personal_information', table => {
+		table.increments('id').primary()
+		table.string('full_name', 40).notNull()
+		table.integer('age', 40).notNull()
+		table.string('city', 40).notNull()
+		table.string('state', 40).notNull()
+		table.string('college').notNull()
+		table.string('campus').notNull()
+		table.string('course', 40).notNull()
+	});
+},
+exports.down = function(knex) {
+    return knex.schema.dropTable('personal_information')
+}

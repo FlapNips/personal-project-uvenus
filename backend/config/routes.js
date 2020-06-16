fd9841f18cab7b0d7e231885e32a90977fd7ea16
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-06-16 09:40:55 -0300

    Update routes.js

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,5 +1,20 @@
 module.exports = app => {
 	app.route('/user')
-		.post(app.api.user.saveOrUpdate)
-		.delete(app.api.user.removeUser)
+		.post(app.api.user.createUser)
+	
+	app.route('/user/:user_id')
+		.put(app.api.user.updateUser)
+		.delete(app.api.user.deletedUser)
+
+	app.route('/category')
+		.post(app.api.category.newCategory)
+
+	app.route('/category/:category_id')
+		.put(app.api.category.updateCategory)
+
+	app.route('/article')
+		.post(app.api.article.newArticle)
+
+	app.route('/article/:article_id')
+		.get(app.api.article.getArticle)
 }
